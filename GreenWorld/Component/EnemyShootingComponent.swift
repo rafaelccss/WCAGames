1b8462d6297778c082374b357db9e856f2112243
Nathan <nathan-batista@hotmail.com>
2022-02-10 10:15:25 -0300

    Criei enemyShotComponent e Entity

---
+++ GreenWorld/Component/EnemyShootingComponent.swift
@@ -0,0 +1,25 @@
+//
+//  EnemyShootingComponent.swift
+//  GreenWorld
+//
+//  Created by Nathan Batista de Oliveira on 10/02/22.
+//
+
+import Foundation
+import GameplayKit
+import SpriteKit
+
+class EnemyShotComponent : GKComponent{
+    var damage = 0
+    var entityManager:EntityManager!
+    
+    init(enemy enemyEntity:Enemy, manager entityManager:EntityManager){
+        self.damage = 25
+        self.entityManager = entityManager
+        super.init()
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+}
