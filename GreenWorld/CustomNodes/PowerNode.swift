37c1809731a5b18f3ff83ffdc1688c23034a643a
Luis <luisfilipe.nascimento11@gmail.com>
2022-02-14 21:55:53 -0300

    added image to power node

--- GreenWorld/CustomNodes/PowerNode.swift
+++ GreenWorld/CustomNodes/PowerNode.swift
@@ -9,12 +9,26 @@ import UIKit
 import SpriteKit
 
 class PowerNode: SKShapeNode {
-    init(origin: CGPoint, size: CGSize, cornerRadius: CGFloat) {
+    
+    let imageNode: SKSpriteNode!
+    let nameNode: SKLabelNode!
+    
+    init(origin: CGPoint, size: CGSize, cornerRadius: CGFloat, power: Powers) {
+        imageNode = SKSpriteNode(imageNamed: power.rawValue)
+        nameNode = SKLabelNode(text: power.rawValue)
         super.init()
         let myPath:CGPath = UIBezierPath(roundedRect: CGRect(origin: origin, size: size),
                                          cornerRadius: cornerRadius).cgPath
         self.path = myPath
-        
+        configureChildrenNodes()
+    }
+    
+    func configureChildrenNodes() {
+        let width = self.frame.width
+        self.imageNode.size = CGSize(width: width, height: self.frame.height * 0.7)
+        self.imageNode.position = CGPoint(x: width / 2, y: 0)
+        self.imageNode.scene?.scaleMode = .aspectFill
+        addChild(self.imageNode)
     }
     
     required init?(coder aDecoder: NSCoder) {
