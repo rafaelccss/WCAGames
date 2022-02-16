621ebca85a700dfceeca7ba54c9b5fe0b06e8b81
Luis <luisfilipe.nascimento11@gmail.com>
2022-02-16 15:56:29 -0300

    change attack texture by power selected

--- GreenWorld/Entity/EnemyShotEntity.swift
+++ GreenWorld/Entity/EnemyShotEntity.swift
@@ -17,7 +17,7 @@ class EnemyShotEntity : GKEntity {
         self.damage = enemyEntity.enemyType == .Boss ? 50 : 25
         self.direction = directionShot
 
-        let spriteComponent = AnimatedSpriteComponent(imageName: enemyEntity.enemyType == EnemyType.Boss ? "Wood" : "Axe", size: CGSize(width: 56, height: 56))
+        let spriteComponent = AnimatedSpriteComponent(imageName: enemyEntity.enemyType == EnemyType.Madeireiro ? "Wood" : "Axe", size: CGSize(width: 56, height: 56))
         spriteComponent.spriteNode.run(.repeatForever(.rotate(byAngle: .pi * 2, duration: 1)))
         
         super.init()
